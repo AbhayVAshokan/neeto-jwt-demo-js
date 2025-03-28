c3407b523c472bf9dc6a11abce24625da4a06259
Abhay V Ashokan <abhayvashokan@gmail.com>
2025-03-28 11:46:16 +0530

    Rename username to email

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -5,17 +5,17 @@ import CurrentUserContext from "../contexts/current-user-context";
 
 export default function LoginPage() {
   const navigate = useNavigate();
-  const [errorText, setErrorText] = useState('');
+  const [errorText, setErrorText] = useState("");
   const { currentUser, setCurrentUser } = useContext(CurrentUserContext);
 
   // users shouldn't be able to see the login page if they are already logged in.
-  // if the currentUser exists in the context, navigate the user to 
+  // if the currentUser exists in the context, navigate the user to
   // the /users/:id page for that user, using the currentUser.id value
   if (currentUser) return <Navigate to={`/users/${currentUser.id}`} />;
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    setErrorText('');
+    setErrorText("");
     const formData = new FormData(event.target);
     const [user, error] = await logUserIn(Object.fromEntries(formData));
     if (error) return setErrorText(error.message);
@@ -23,18 +23,30 @@ export default function LoginPage() {
     navigate(`/users/${user.id}`);
   };
 
-  return <>
-    <h1>Login</h1>
-    <form onSubmit={handleSubmit} aria-labelledby="login-heading">
-      <h2 id='login-heading'>Log back in!</h2>
-      <label htmlFor="username">Username</label>
-      <input type="text" autoComplete="username" id="username" name="username" />
+  return (
+    <>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit} aria-labelledby="login-heading">
+        <h2 id="login-heading">Log back in!</h2>
+        <label htmlFor="username">Email</label>
+        <input
+          type="text"
+          autoComplete="username"
+          id="username"
+          name="username"
+        />
 
-      <label htmlFor="password">Password</label>
-      <input type="password" autoComplete="current-password" id="password" name="password" />
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          autoComplete="current-password"
+          id="password"
+          name="password"
+        />
 
-      <button>Log in!</button>
-    </form>
-    {!!errorText && <p>{errorText}</p>}
-  </>;
+        <button>Log in!</button>
+      </form>
+      {!!errorText && <p>{errorText}</p>}
+    </>
+  );
 }
